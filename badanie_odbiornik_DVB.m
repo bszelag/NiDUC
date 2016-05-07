d019ac38fa24089012442f11fbc0f182bdd1026a
Szymon Wojciechowski <bluetapeon@gmail.com>
2016-05-07 20:01:23 +0200

    Poprawienie odbiornika i nadajnika DVB
    najwyraźniej nie były umieszczone poprawne pliki.

--- badanie_odbiornik_DVB.m
+++ badanie_odbiornik_DVB.m
@@ -1,11 +1,13 @@
 s = round(rand(1,1000));
 k = round(rand(1,16));
-w = [8 8 8 8 8 8 8 8 8 8 8 8];
+w = [8 8 8 8 8];
 
-S = nadajnik_DVB(s,k,w,40);
-S1 = odbiornik_DVB(S,k,w);
+S = nadajnik_DVB(s,k,w,10);
+S1 = odbiornik_DVB(S,k,w,5);
 
-isequal(s,S1)
+figure(1)
+hist(repetitions(S))
 
+figure(2)
 stem(s - S1)
-title('Roznica sygnalow: s - S');
+title('Roznica sygnalow: s - S1');
